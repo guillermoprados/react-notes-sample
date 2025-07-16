42adf9c9549577aa4f6e4ed0a75cdc1edf67728e
Guille <guillermoprados@gmail.com>
2025-07-16 18:29:36 -0300

    [FE] - Home: Added a basic topbar

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import TopBar from '../components/TopBar';
+
 function Home() {
   return (
-    <div>
-      Home
+    <div className="h-screen w-screen bg-gray-50 flex flex-col">
+      <TopBar />
+      <div className="flex-1 p-6 overflow-y-auto">
+        <p>Notes</p>
+      </div>
     </div>
   );
 }
